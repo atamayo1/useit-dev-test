8d148ad6e90d5d9c362cedd7c3a505f1839b181c
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2019-12-02 02:22:38 -0500

    add login, register, recover

--- App.js
+++ App.js
@@ -1,62 +1,45 @@
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import LoadingScreen from "./screens/LoadingScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RecoverScreen from "./screens/RecoverScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import HomeScreen from "./screens/HomeScreen";
 
-import AppNavigator from './navigation/AppNavigator';
+import * as firebase from 'firebase';
 
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
 
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
-    );
-  }
-}
+var firebaseConfig = {
+  apiKey: "AIzaSyB9W8ZTLtOnGcXkVEPLWlpwxr6ZcHBpn5Y",
+  authDomain: "useitweb-4ac23.firebaseapp.com",
+  databaseURL: "https://useitweb-4ac23.firebaseio.com",
+  projectId: "useitweb-4ac23",
+  storageBucket: "",
+  messagingSenderId: "500966015618",
+  appId: ""
+};
 
-async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
-  ]);
-}
+firebase.initializeApp(firebaseConfig);
 
-function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
-}
-
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
+const AppStack = createStackNavigator({
+  Home: HomeScreen
 });
+
+const AuthStack = createStackNavigator({
+  Login: LoginScreen,
+  Register: RegisterScreen,
+  Recover: RecoverScreen
+});
+
+export default createAppContainer(
+    createSwitchNavigator(
+        {
+          Loading: LoadingScreen,
+          App: AppStack,
+          Auth: AuthStack
+        },
+        {
+          initialRouteName: "Loading"
+        }
+    )
+);
